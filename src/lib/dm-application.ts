39e6d1cd0e0744bedfd534cc6f19aab698fffc97
Ryan Crockett <ryan.crockett8@gmail.com>
2020-09-06 12:18:17 -0400

    Better time out handling on first message in dm

--- src/lib/dm-application.ts
+++ src/lib/dm-application.ts
@@ -4,22 +4,28 @@ export const startDmApp = async (message: Message) => {
   const dm = await message.author.createDM()
   await dm.send('Hey there! Type `!start` to start your application, and `!end` to exit the process without applying at any time.')
 
-  await dm.awaitMessages(
-    (message: Message) => message.content === '!start' || message.content === '!end',
-    {
-      time: 60000,
-      max: 1
+  try {
+    const startMessage = await dm.awaitMessages(
+      (message: Message) => message.content === '!start' || message.content === '!end',
+      {
+        time: 60000,
+        max: 1
+      }
+    )
+      .then(collection => collection.first())
+      .catch(() => {
+        throw new Error('Timed Out')
+      })
+    
+    if (startMessage.content === '!start') {
+      await dm.send('Starting Application Process')
+    } else {
+      await dm.send('Ending Application Process')
+      await dm.delete('Ended By User')
     }
-  )
-    .then(async (collected) => {
-      const message = collected.first()
-      if (message.content === '!start') {
-        await dm.send('Starting application!')
-      }
-      if (message.content === '!end') {
-        await message.reply('Ended application early.')
-        await dm.delete('Ending Application')
-      }
-    })
-    .catch(() => dm.delete('Took too long to respond.'))
+  } catch (e) {
+    await dm.delete('Timed Out')
+  }
+
+
 }
